2c34f11a5786b3a7b32a2f22cf89bb0651bb4190
Shannon Ni <sni@zymergen.com>
2019-02-15 16:03:34 -0800

    Add filters

---
+++ src/components/home/filter.js
@@ -0,0 +1,355 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import Select from "react-select";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import NoSsr from "@material-ui/core/NoSsr";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+import Chip from "@material-ui/core/Chip";
+import MenuItem from "@material-ui/core/MenuItem";
+import CancelIcon from "@material-ui/icons/Cancel";
+import { emphasize } from "@material-ui/core/styles/colorManipulator";
+import Grid from "@material-ui/core/Grid";
+import Button from '@material-ui/core/Button';
+
+const processDefKeys = [
+  { label: "B0001" },
+  { label: "B0002" },
+  { label: "B0003" },
+  { label: "C0001" },
+  { label: "C0002" },
+  { label: "E0018" },
+  { label: "E1009" },
+  { label: "F1029" },
+  { label: "F2030" },
+  { label: "W1008" }
+].map(suggestion => ({
+  value: suggestion.label,
+  label: suggestion.label
+}));
+
+const callActivityIds = [
+  { label: "B0001_call1" },
+  { label: "B0001_call2" },
+  { label: "B0003_call1" },
+  { label: "B0003_call2" },
+  { label: "C0001_call1" },
+  { label: "C0001_call2" },
+  { label: "C0002_call1" },
+  { label: "E0018_call1" },
+  { label: "E1009_call1" },
+  { label: "F1029_call1" },
+  { label: "F2030_call1" },
+  { label: "W1008_call1" }
+].map(suggestion => ({
+  value: suggestion.label,
+  label: suggestion.label
+}));
+
+const multiInstanceIds = [
+  { label: "multi_instance_1" },
+  { label: "multi_instance_2" }
+].map(suggestion => ({
+  value: suggestion.label,
+  label: suggestion.label
+}));
+
+const parallelGatewayIds = [
+  { label: "parallel_gateway_1" },
+  { label: "parallel_gateway_2" }
+].map(suggestion => ({
+  value: suggestion.label,
+  label: suggestion.label
+}));
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    height: 250
+  },
+  input: {
+    display: "flex",
+    padding: 0
+  },
+  valueContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    flex: 1,
+    alignItems: "center",
+    overflow: "hidden"
+  },
+  chip: {
+    margin: `${theme.spacing.unit / 2}px ${theme.spacing.unit / 4}px`
+  },
+  chipFocused: {
+    backgroundColor: emphasize(
+      theme.palette.type === "light"
+        ? theme.palette.grey[300]
+        : theme.palette.grey[700],
+      0.08
+    )
+  },
+  noOptionsMessage: {
+    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`
+  },
+  singleValue: {
+    fontSize: 16
+  },
+  placeholder: {
+    position: "absolute",
+    left: 2,
+    fontSize: 16
+  },
+  paper: {
+    position: "absolute",
+    zIndex: 1,
+    marginTop: theme.spacing.unit,
+    left: 0,
+    right: 0
+  },
+  divider: {
+    height: theme.spacing.unit * 2
+  }
+});
+
+function NoOptionsMessage(props) {
+  return (
+    <Typography
+      color="textSecondary"
+      className={props.selectProps.classes.noOptionsMessage}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Typography>
+  );
+}
+
+function inputComponent({ inputRef, ...props }) {
+  return <div ref={inputRef} {...props} />;
+}
+
+function Control(props) {
+  return (
+    <TextField
+      fullWidth
+      InputProps={{
+        inputComponent,
+        inputProps: {
+          className: props.selectProps.classes.input,
+          inputRef: props.innerRef,
+          children: props.children,
+          ...props.innerProps
+        }
+      }}
+      {...props.selectProps.textFieldProps}
+    />
+  );
+}
+
+function Option(props) {
+  return (
+    <MenuItem
+      buttonRef={props.innerRef}
+      selected={props.isFocused}
+      component="div"
+      style={{
+        fontWeight: props.isSelected ? 500 : 400
+      }}
+      {...props.innerProps}
+    >
+      {props.children}
+    </MenuItem>
+  );
+}
+
+function Placeholder(props) {
+  return (
+    <Typography
+      color="textSecondary"
+      className={props.selectProps.classes.placeholder}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Typography>
+  );
+}
+
+function SingleValue(props) {
+  return (
+    <Typography
+      className={props.selectProps.classes.singleValue}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Typography>
+  );
+}
+
+function ValueContainer(props) {
+  return (
+    <div className={props.selectProps.classes.valueContainer}>
+      {props.children}
+    </div>
+  );
+}
+
+function MultiValue(props) {
+  return (
+    <Chip
+      tabIndex={-1}
+      label={props.children}
+      className={classNames(props.selectProps.classes.chip, {
+        [props.selectProps.classes.chipFocused]: props.isFocused
+      })}
+      onDelete={props.removeProps.onClick}
+      deleteIcon={<CancelIcon {...props.removeProps} />}
+    />
+  );
+}
+
+function Menu(props) {
+  return (
+    <Paper
+      square
+      className={props.selectProps.classes.paper}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Paper>
+  );
+}
+
+const components = {
+  Control,
+  Menu,
+  MultiValue,
+  NoOptionsMessage,
+  Option,
+  Placeholder,
+  SingleValue,
+  ValueContainer
+};
+
+class Filters extends React.Component {
+  state = {
+    single: null,
+    multi: null
+  };
+
+  handleChange = name => value => {
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    const { classes, theme } = this.props;
+    this.state = {
+        filter: {
+        }
+    }
+
+    const selectStyles = {
+      input: base => ({
+        ...base,
+        color: theme.palette.text.primary,
+        "& input": {
+          font: "inherit"
+        }
+      })
+    };
+
+    return (
+      <Grid container spacing={24}>
+        <Grid item xs={2} className={classes.root}>
+          <NoSsr>
+            <Select
+              classes={classes}
+              styles={selectStyles}
+              options={processDefKeys}
+              components={components}
+              value={this.state.filter.processDefKey}
+              onChange={this.handleChange("multi")}
+              placeholder="Process Def Key"
+              isMulti
+            />
+          </NoSsr>
+        </Grid>
+        <Grid item xs={2} className={classes.root}>
+          <NoSsr>
+            <Select
+              classes={classes}
+              styles={selectStyles}
+              options={callActivityIds}
+              components={components}
+              value={this.state.filter.fromCallActivityId}
+              onChange={this.handleChange("multi")}
+              placeholder="From Call Activity ID"
+              isMulti
+            />
+          </NoSsr>
+        </Grid>
+        <Grid item xs={2} className={classes.root}>
+          <NoSsr>
+            <Select
+              classes={classes}
+              styles={selectStyles}
+              options={multiInstanceIds}
+              components={components}
+              value={this.state.filter.multiInstanceId}
+              onChange={this.handleChange("multi")}
+              placeholder="Multi Instance ID"
+              isMulti
+            />
+          </NoSsr>
+        </Grid>
+        <Grid item xs={2} className={classes.root}>
+          <NoSsr>
+            <Select
+              classes={classes}
+              styles={selectStyles}
+              options={parallelGatewayIds}
+              components={components}
+              value={this.state.filter.parallelGatewayId}
+              onChange={this.handleChange("multi")}
+              placeholder="Parallel Gateway ID"
+              isMulti
+            />
+          </NoSsr>
+        </Grid>
+        <Grid item xs={2} className={classes.root}>
+          <NoSsr>
+            <Select
+              classes={classes}
+              styles={selectStyles}
+              options={callActivityIds}
+              components={components}
+              value={this.state.filter.toCallActivityId}
+              onChange={this.handleChange("multi")}
+              placeholder="To Call Activity ID"
+              isMulti
+            />
+          </NoSsr>
+        </Grid>
+        <Grid item>
+            <Button
+              color='primary'
+              className={classes.button}
+              onClick={this.toggleFilters}
+            >
+              Submit
+            </Button>
+          </Grid>
+      </Grid>
+    );
+  }
+}
+
+Filters.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired
+};
+
+export default withStyles(styles, { withTheme: true })(Filters);
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,9 +1,148 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import MenuItem from '@material-ui/core/MenuItem';
+import Collapse from '@material-ui/core/Collapse';
+import Select from '@material-ui/core/Select';
+import Filters from './filter';
 
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
+const styles = theme => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400
+  },
+  input: {
+    marginLeft: 8,
+    flex: 1
+  },
+  iconButton: {
+    padding: 10
+  },
+  gridContainer: {
+    flexGrow: 1,
+    marginBottom: 15
+  },
+  formControl: {
+    margin: 10,
+    minWidth: 120
+  },
+  valueContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flex: 1,
+    alignItems: 'center',
+    overflow: 'hidden'
+  }
+});
 
-export default Home;
+class Home extends React.Component {
+  state = {
+    openFilter: false,
+    sortBy: 'default'
+  };
+
+  changeSortBy = event => {
+    this.setState({
+      sortBy: event.target.value
+    });
+  };
+
+  onProcessModelFilterChange = name => value => {
+    this.setState({
+      [name]: value
+    });
+  };
+
+  toggleFilters = () => {
+    this.setState(state => ({ openFilter: !state.openFilter }));
+  };
+  render() {
+    const { classes, theme } = this.props;
+    const { openFilter, processModelFilter } = this.state;
+
+    return (
+      <div>
+        <Grid container spacing={24} className={classes.gridContainer}>
+          <Grid item>
+            <Paper className={classes.root} elevation={1}>
+              <InputBase className={classes.input} placeholder='Search' />
+              <IconButton className={classes.iconButton} aria-label='Search'>
+                <SearchIcon />
+              </IconButton>
+            </Paper>
+          </Grid>
+          <Grid item>
+            <Select
+              value={this.state.sortBy}
+              onChange={this.changeSortBy}
+              name='Sort By'
+              inputProps={{
+                name: 'sortBy',
+                id: 'sortBy-simple'
+              }}
+            >
+              <MenuItem value={'default'}>
+                <em>Sort By: Default</em>
+              </MenuItem>
+              <MenuItem value={'createDate'}>Sort By: Create Date</MenuItem>
+              <MenuItem value={'name'}>Sort By: Name</MenuItem>
+              <MenuItem value={'creator'}>Sort By: Creator</MenuItem>
+            </Select>
+          </Grid>
+          <Grid item>
+            <Button
+              color='primary'
+              className={classes.button}
+              onClick={this.toggleFilters}
+            >
+              Filters
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid container className={classes.gridContainer}>
+          <Grid item xs={12}>
+            <Collapse in={openFilter}>
+              <Filters />
+              <Paper elevation={4} className={classes.paper}>
+                {/* <form className={classes.root} autoComplete='off'>
+                  <FormControl className={classes.formControl}>
+                    <InputLabel htmlFor='age-simple'>Age</InputLabel>
+                    <Select
+                      value={this.state.age}
+                      onChange={this.handleChange}
+                      inputProps={{
+                        name: 'age',
+                        id: 'age-simple'
+                      }}
+                    >
+                      <MenuItem value=''>
+                        <em>None</em>
+                      </MenuItem>
+                      <MenuItem value={10}>Ten</MenuItem>
+                      <MenuItem value={20}>Twenty</MenuItem>
+                      <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                  </FormControl>{' '}
+                </form> */}
+              </Paper>
+            </Collapse>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+Home.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles, { withTheme: true })(Home);
